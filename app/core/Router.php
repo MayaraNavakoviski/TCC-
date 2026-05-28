b5d38d5a6100a84e67dad7fe29f54dc646bd4c5e
Mayara Navakoviski <mayara.navakoviski.tds2023@gmail.com>
2026-05-28 11:33:59 -0300

    Create Router.php

---
+++ app/core/Router.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace app\core;
+
+use Exception;
+
+class Router
+{
+    private array $routes = [];
+
+    public function get(string $route, string $action): void
+    {
+        $this->routes[] = [
+            'method' => 'get',
+            'route'  => $route,
+            'action' => $action,
+        ];
+    }
+
+    public function post(string $route, string $action): void
+    {
+        $this->routes[] = [
+            'method' => 'post',
+            'route'  => $route,
+            'action' => $action,
+        ];
+    }
+
+    public function run(): void
+    {
+        $uri    = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        $method = strtolower($_SERVER['REQUEST_METHOD']);
+
+        foreach ($this->routes as $route) {
+            if ($route['route'] === $uri && $route['method'] === $method) {
+                $this->dispatch($route);
+                return;
+            }
+        }
+
+        http_response_code(404);
+        exit('Rota não encontrada');
+    }
+
+    private function dispatch(array $route): void
+    {
+        [$controller, $method] = explode('@', $route['action']);
+
+        $controllerClass = "app\\controllers\\$controller";
+
+        if (!class_exists($controllerClass)) {
+            print "Controller $controller não encontrado";
+            die;
+        }
+
+        if (!method_exists($controllerClass, $method)) {
+            print "Método $method não encontrado em $controllerClass";
+            die;
+        }
+
+        $instance = new $controllerClass;
+        $instance->$method();
+    }
+
+    public function getAllRoutes(): array
+    {
+        return $this->routes;
+    }
+}
